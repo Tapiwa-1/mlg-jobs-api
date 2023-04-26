d9de5490b7c9f0790732a05c09a929a198887ac0
tapiwa-1 <tmotsi10@gmail.com>
2023-04-26 14:15:24 +0200

    add breeze api

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['auth:sanctum'])->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Route::get('/logged-in-user', [UserController::class, 'loggedInUser']);
+    // Route::post('/update-user-image', [UserController::class, 'updateUserImage']);
+    // // Route::patch('/update-user', [UserController::class, 'updateUser']);
+    // Route::get('/users',[UsersController::class, 'users']);
+
 });
+
