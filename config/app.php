df8388f761dde882c8c16a0c312f9764d21e931c
tapiwa-1 <tmotsi10@gmail.com>
2023-05-02 10:40:30 +0200

    working on roles

--- config/app.php
+++ config/app.php
@@ -189,6 +189,7 @@ return [
         /*
          * Package Service Providers...
          */
+         Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
